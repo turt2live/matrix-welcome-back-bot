9a292620534a9f1b67f99eb9f1956d901a7dd5dc
Travis Ralston <travpc@gmail.com>
2018-03-29 21:33:01 -0600

    Send a pilled message

--- src/WelcomeHandler.js
+++ src/WelcomeHandler.js
@@ -34,7 +34,20 @@ class WelcomeHandler {
         var lastActiveMs = userTree[event.getSender()];
         if (lastActiveMs && (moment().valueOf() - lastActiveMs) > (WelcomeStore.getWelcomeTimeout(event.getRoomId()) * 60000)) {
             LogService.info("WelcomeHandler", "Welcoming " + event.getSender() + " back in room " + event.getRoomId());
-            this._client.sendNotice(event.getRoomId(), "Welcome back, " + event.getSender());
+            this._client.getProfileInfo(event.getSender(), "displayname").then(result => {
+                if (!result.displayname) result.displayname = event.getSender();
+                const pilled = 'Welcome back, <a href="https://matrix.to/#/' + event.getSender() + '">' + result.displayname + "</a>";
+                const plain = "Welcome back, " + result.displayname;
+                this._client.sendMessage(event.getRoomId(), {
+                    msgtype: "m.notice",
+                    body: plain,
+                    format: "org.matrix.custom.html",
+                    formatted_body: pilled,
+                });
+            }).catch(err => {
+                LogService.error("WelcomeHandler", err);
+                this._client.sendNotice(event.getRoomId(), "Welcome back, " + event.getSender());
+            });
         } else LogService.info("WelcomeHandler", "User " + event.getSender() + " was active recently in " + event.getRoomId());
         userTree[event.getSender()] = moment().valueOf();
         WelcomeStore.storeLastActiveMap(this._recentUsers);
